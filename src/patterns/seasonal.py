6602a0e2b758d8c4709a0a590eded24b7695460c
Caleb <c4leb.johnson@gmail.com>
2018-11-16 14:14:06 -0800

    more tweaks

--- src/patterns/seasonal.py
+++ src/patterns/seasonal.py
@@ -7,34 +7,18 @@ from utils import to_color, mult_color, blend_color, wheel
 class seasonal(base):
     def __init__(self, numPixels):
         super(seasonal, self).__init__(numPixels)
-        self.full_stop = True
-        self.trunk_end = numPixels / 4
+        self.trunk_end = numPixels / 5
         self.trunk_taper = 50
         self.noise_color = [0] * numPixels
         self.noise_bri = [1.0] * numPixels
 
-    # def clear(self):
-        # self.i = 0
-        # self.cleared = 0
-        # shuffle(self.strip_order)
-
     def _step(self, state, strip):
         for pos in range(self.numPx):
-            # if self.i >= len(self.strip_order):
-            #     self.i = 0
-            #     if state == State.START:
-            #         return State.RUNNING
-            # if self.i == 0 and state == State.STOP:
-            #     if self.cleared == 2:
-            #         self.cleared = 0
-            #         return State.OFF
-            #     self.cleared += 1
-            # pos = self.strip_order[self.i]
 
             # blend from base to leaves
-            if randint(0, 10) == 0:
+            if randint(0, 3) == 0:
                 self.noise_color[pos] = min(30, max(0, self.noise_color[pos] + randint(-1,1)))
-            self.noise_bri[pos] = min(1.0, max(0.0, self.noise_bri[pos] + (random() - 0.499) / 10.0))
+            self.noise_bri[pos] = min(1.0, max(0.0, self.noise_bri[pos] + (random() - 0.499) / 5.0))
 
             base_color = to_color(240,200,120)
             leaf_color = mult_color(wheel(abs((int(time() * 2.0)) % 150 - 75) + self.noise_color[pos]), self.noise_bri[pos])
@@ -42,14 +26,13 @@ class seasonal(base):
             if state == State.STOP:
                 px_color = 0x0
             if pos < self.trunk_end:
-                px_color = base_color
+                px_color = mult_color(base_color, self.noise_bri[pos])
             elif pos < self.trunk_end + self.trunk_taper:
-                px_color = blend_color(leaf_color, base_color, (pos - self.trunk_end) / float(self.trunk_taper))
+                px_color = blend_color(leaf_color, mult_color(base_color, self.noise_bri[pos]), float(pos - self.trunk_end) / float(self.trunk_taper))
             else:
                 px_color = leaf_color
 
             strip._led_data[pos] = px_color
-            # self.i += 1
 
         if state == State.START:
             return State.RUNNING
