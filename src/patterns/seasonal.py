ca6234d5e63c17677ac57fe0e1ff5e1bb2fc969b
Caleb <c4leb.johnson@gmail.com>
2018-11-16 14:03:15 -0800

    more tweaks

--- src/patterns/seasonal.py
+++ src/patterns/seasonal.py
@@ -9,31 +9,32 @@ class seasonal(base):
         super(seasonal, self).__init__(numPixels)
         self.full_stop = True
         self.trunk_end = numPixels / 4
-        self.trunk_taper = 30
+        self.trunk_taper = 50
         self.noise_color = [0] * numPixels
         self.noise_bri = [1.0] * numPixels
 
     def clear(self):
-        self.i = 0
-        self.cleared = 0
-        shuffle(self.strip_order)
+        # self.i = 0
+        # self.cleared = 0
+        # shuffle(self.strip_order)
 
     def _step(self, state, strip):
-        for t in range(10):
-            if self.i >= len(self.strip_order):
-                self.i = 0
-                if state == State.START:
-                    return State.RUNNING
-            if self.i == 0 and state == State.STOP:
-                if self.cleared == 2:
-                    self.cleared = 0
-                    return State.OFF
-                self.cleared += 1
-            pos = self.strip_order[self.i]
+        for pos in range(self.numPx):
+            # if self.i >= len(self.strip_order):
+            #     self.i = 0
+            #     if state == State.START:
+            #         return State.RUNNING
+            # if self.i == 0 and state == State.STOP:
+            #     if self.cleared == 2:
+            #         self.cleared = 0
+            #         return State.OFF
+            #     self.cleared += 1
+            # pos = self.strip_order[self.i]
 
             # blend from base to leaves
-            self.noise_color[pos] = min(30, max(0, self.noise_color[pos] + randint(-1,1)))
-            self.noise_bri[pos] = min(1.0, max(0.0, self.noise_bri[pos] + (random() - 0.499) / 4.0))
+            if randint(0, 10) == 0:
+                self.noise_color[pos] = min(30, max(0, self.noise_color[pos] + randint(-1,1)))
+            self.noise_bri[pos] = min(1.0, max(0.0, self.noise_bri[pos] + (random() - 0.499) / 10.0))
 
             base_color = to_color(240,200,120)
             leaf_color = mult_color(wheel(abs((int(time() * 2.0)) % 150 - 75) + self.noise_color[pos]), self.noise_bri[pos])
@@ -48,6 +49,11 @@ class seasonal(base):
                 px_color = leaf_color
 
             strip._led_data[pos] = px_color
-            self.i += 1
+            # self.i += 1
+
+        if state == State.START:
+            return State.RUNNING
+        elif state == State.STOP:
+            return State.OFF
 
         return state
